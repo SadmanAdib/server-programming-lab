f1f4845607f2245936906f7caf5ed60f0d2b8845
SadmanAdib <56638634+SadmanAdib@users.noreply.github.com>
2021-05-07 00:47:13 +0600

    Completed 'utils' module

--- Lab_01_180042106/utils.js
+++ Lab_01_180042106/utils.js
@@ -2,3 +2,13 @@ const HelloFunction = require("./helloWorld");
 
 console.log(HelloFunction.Hello);
 console.log(HelloFunction.name);
+
+setInterval(()=>
+{
+    HelloFunction.Hello();
+}, 1000); //Runs continuously but periodically with the specified time interval
+
+setTimeout(()=>
+{
+    console.log(HelloFunction.name);
+}, 5000);  //Runs only once after the specified time.
