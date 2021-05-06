da1b4a1b485d690699290921eacc25f5ca6560b4
SadmanAdib <56638634+SadmanAdib@users.noreply.github.com>
2021-05-07 00:08:25 +0600

    Completed 'globalVariables' module

---
+++ Lab_01_180042106/globalVariables.js
@@ -0,0 +1,5 @@
+console.log(__dirname); //shows the path to the current directory.
+console.log(__filename); //shows the name of the file.
+console.log(require("fs")); //function to use module.
+console.log(module);
+console.log(process); //info about where the process is being executed.
